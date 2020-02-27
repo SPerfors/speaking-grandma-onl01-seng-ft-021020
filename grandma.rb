472618f9c87924675df8cf2b1ad36e528d0e0c51
Steven Perfors <stevecanbuild@gmail.com>
2020-02-27 17:47:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     puts 'HUH?! SPEAK UP, SONNY!'
-  elseif phrase == phrase.upcase
+  elsif phrase == phrase.upcase
     puts 'NO, NOT SINCE 1938!'
-  elseif phrase == 'I LOVE YOU GRANDMA!'
+  elsif phrase == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
   end 
 end
